d8f2fc4b1eda9f9cb7e1397f1cf31bab190c8e73
RohithKannan <51419779+RohithKKannan@users.noreply.github.com>
2023-06-02 00:26:50 +0530

    Get velocity implemented in paddle
    	- Get Velocity method created to return velocity of paddle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,13 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float maxX = 7.6f;
     [SerializeField] float speed = 10f;
+    Vector3 prev;
+    Vector2 displacement, velocity;
     float xVal;
     void PlayerInput()
     {
         xVal = Input.GetAxisRaw("Horizontal");
+        prev = transform.position;
     }
     void Update()
     {
@@ -16,5 +19,11 @@ public class PlayerController : MonoBehaviour
         {
             transform.Translate(Vector3.right * xVal * speed * Time.deltaTime);
         }
+        displacement = transform.position - prev;
+        velocity = displacement / Time.deltaTime;
+    }
+    public Vector2 GetVelocity()
+    {
+        return velocity;
     }
 }
